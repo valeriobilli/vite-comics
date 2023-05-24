c6156e0c8b519a3a7d5851fec1bbff0412c3194c
Valerio Billi <valerio.billi18@gmail.com>
2023-05-24 04:19:08 +0200

    Add component AppContent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppContent from './components/AppContent.vue'
 
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    AppContent
   }
 }
 </script>
 
 <template>
   <AppHeader />
+  <AppContent />
 </template>
 
 <style scoped></style>
---
+++ src/components/AppContent.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "AppContent"
+}
+</script>
+
+<template>
+    <div class="container">
+        contenuto
+    </div>
+</template>
+
+<style scoped>
+.container {
+    min-height: 130px;
+    background-color: black;
+    color: white;
+}
+</style>
