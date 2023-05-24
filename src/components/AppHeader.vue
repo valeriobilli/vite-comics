eccde9ce8d573cf09220853ba1194cf68f2096eb
Valerio Billi <valerio.billi18@gmail.com>
2023-05-24 04:44:14 +0200

    content and style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,13 +5,44 @@ export default {
 </script>
 
 <template>
-    <header>Header</header>
+    <header>
+        <div class="container">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <div class="navbar">
+            <a href="">CHARACTERS</a>
+            <a href="">COMICS</a>
+            <a href="">MOVIES</a>
+            <a href="">TV</a>
+            <a href="">GAMES</a>
+            <a href="">COLLECTIBLES</a>
+            <a href="">VIDEOS</a>
+            <a href="">FANS</a>
+            <a href="">NEWS</a>
+            <a href="">SHOP</a>
+        </div>
+    </header>
 </template>
 
 <style scoped>
 header {
     height: 120px;
     background-color: white;
+    width: 70%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.navbar {
+    display: flex;
+}
+
+.navbar>a {
+    margin-right: 1rem;
+    text-decoration: none;
+    color: black;
 }
 </style>
 
